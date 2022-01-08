791955b008bf174906d96b46c1d728c69d8d81e4
721945 <this721945@gmail.com>
2022-01-08 15:17:19 +0700

    initial project 😎

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// import rest " application/rest/"
+// import rest "application/rest"
+
+func main() {
+	// r := gin.Default()
+	// rest.Run()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+
+	// r.Run()
+}
